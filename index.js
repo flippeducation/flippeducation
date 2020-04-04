06d3162189005b111ceb3ccc8ac24ef5f64d0571
psvenk <45520974+psvenk@users.noreply.github.com>
2020-04-04 17:17:43 -0400

    Initialize website

---
+++ index.js
@@ -0,0 +1,47 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const port = 3000;
+
+app.set("view engine", "pug");
+app.set("views", path.join(__dirname, "views"));
+
+app.listen(port, () => console.log(`App listening at http://localhost:${port}`));
+
+// Expose frontend dependencies from node-modules
+// https://stackoverflow.com/a/27464258
+new Map([
+  [
+    "/",
+    "/public/"
+  ],
+  [
+    "/css/pure-min.css",
+    "/node_modules/purecss/build/pure-min.css"
+  ],
+  [
+    "/css/grids-responsive-min.css",
+    "/node_modules/purecss/build/grids-responsive-min.css"
+  ],
+]).forEach((location, endpoint) => {
+  if (location.slice(-1) === "/") app.use(
+      endpoint, express.static(path.join(__dirname, location))
+  );
+  else app.get(endpoint, (req, res) => {
+      res.sendFile(path.join(__dirname, location));
+  });
+});
+
+const paths = new Map([
+  ["/", "Home"],
+  ["/search", "Search"],
+  ["/submit", "Submit Videos"]
+]);
+
+app.get("/", (req, res) => {
+  res.render("index", {
+    page: "/",
+    paths: paths
+  });
+});
