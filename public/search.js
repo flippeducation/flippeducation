b1cf3a3abed51288999910bdfc19c18895b77475
notrodes <reallystupidid@gmail.com>
2020-04-05 14:56:48 -0400

    Added to the search page, created a search bar and added extra parameters with the advanced search button.

---
+++ public/search.js
@@ -0,0 +1,20 @@
+let state = {
+    advancedOptionsToggle: false
+};
+
+function showHideAdvancedOptions() {
+    const advancedOptions = document.getElementById("advanced_options");
+    const submit1 = document.getElementById("submit1");
+    const submit2 = document.getElementById("submit2");
+    if (state.advancedOptions) {
+        submit1.style.display =  "inline";
+        submit2.style.display = "none";
+        advancedOptions.style.display = "none";
+        state.advancedOptions = false
+    } else if (!state.advancedOptions) {
+        submit1.style.display =  "none";
+        submit2.style.display = "inline";
+        advancedOptions.style.display = "block";
+        state.advancedOptions = true
+    }
+}
