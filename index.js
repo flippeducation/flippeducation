abb64e266011319b2ed62a4c52f5d1cbe2f37cbc
psvenk <45520974+psvenk@users.noreply.github.com>
2020-04-05 09:57:50 -0400

    Fix 404 page to give status 404

--- index.js
+++ index.js
@@ -46,9 +46,11 @@ app.get("/", (req, res) => {
   });
 });
 
-app.get("*", (req, res) => {
+app.use((req, res) => {
+  res.status(404);
   res.render("404", {
     page: "/404",
+    title: "404",
     paths: paths
-  })
+  });
 });
