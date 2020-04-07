5d9e5e4628e9b004736b923b6383165d219ba48e
psvenk <45520974+psvenk@users.noreply.github.com>
2020-04-06 20:24:22 -0400

    Add some basic MariaDB connector code

--- index.js
+++ index.js
@@ -1,11 +1,22 @@
 const express = require("express");
 const pathLib = require("path");
+const mariadb = require("mariadb");
+const bodyParser = require("body-parser");
+
+// Allow environment variables to be passed from a file called ".env"---
+// this is so that database credentials (DB_USER and DB_PWD)
+// can be stored for convenience.
+// The file .env is in .gitignore and so should not be committed.
+require("dotenv").config();
 
 const app = express();
 const port = 3000;
 
 app.set("view engine", "pug");
 app.set("views", pathLib.join(__dirname, "views"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+// https://stackoverflow.com/a/38763341
 
 app.listen(port, async () => console.log(`App listening at http://localhost:${port}`));
 
@@ -71,6 +82,35 @@ for (const [path, {view, title,}] of pages) {
     });
   });
 }
+
+// Create database connection pool if username and password are given
+let pool;
+if (process.env.DB_USER && process.env.DB_PWD) {
+  pool = mariadb.createPool({
+    host: process.env.DB_HOST || "localhost",
+    user: process.env.DB_USER,
+    password: process.env.DB_PWD,
+    connectionLimit: 5
+  });
+}
+else {
+  console.error("Proceeding without database connection");
+}
+
+app.post("/submit", async (req, res) => {
+  let conn;
+  try {
+    conn = await pool.getConnection();
+    console.log(req.body);
+  }
+  catch (err) {
+    console.error("Error at /submit endpoint");
+  }
+  finally {
+    if (conn) conn.release();
+  }
+  res.redirect("/");
+});
 
 app.use(async (req, res) => {
   res.status(404);
